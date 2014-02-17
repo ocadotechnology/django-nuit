faf1d2e3712bcc3cf08d812e1c8ed8056e191fd1
Ben Cardy <ben.cardy@ocado.com>
2014-02-17 09:38:57 +0000

    Added handlers

---
+++ nuit/handlers.py
@@ -0,0 +1,16 @@
+'''Nuit Status Code Handlers'''
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.shortcuts import render
+
+def generic_handler(request, template, status, context={}):
+    return render(request, template, context, status)
+
+def handler500(request, template='nuit/generic/500.html'):
+    return generic_handler(request, template, 500)
+
+def handler403(request, template='nuit/generic/403.html'):
+    return generic_handler(request, template, 403)
+
+def handler404(request, template='nuit/generic/404.html'):
+    return generic_handler(request, template, 404)
