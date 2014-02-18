5f18940a1d596d47f67068d92327ea9e3c349714
Ben Cardy <ben.cardy@ocado.com>
2014-02-18 10:22:26 +0000

    Added more docs

--- docs/conf.py
+++ docs/conf.py
@@ -94,11 +94,11 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
