cf412c3a8ee438494911f906d204e9322cfeada6
Ben Cardy <ben.cardy@ocado.com>
2014-02-20 13:09:28 +0000

    Ability to size prefix and postfix

--- nuit/templatetags/nuit.py
+++ nuit/templatetags/nuit.py
@@ -21,12 +21,16 @@ def is_quoted(string):
 def set_active_menu(active_menu):
     '''
     Inserts a span with a class of .nuit-active-menu that is picked up by Javascript
-    to highlight the correct menu item..
+    to highlight the correct menu item.
     '''
     return "<span style='display: none' class='nuit-active-menu'>%s</span>" % active_menu
 
 
 class ExtendNode(template.Node):
+    '''
+    Template node that extends another template with additional variables.
+    '''
+
     def __init__(self, node, kwargs):
         self.node = node
         self.kwargs = dict(("nuit_%s" % key, value) for key, value in kwargs.iteritems())
@@ -41,15 +45,19 @@ class ExtendNode(template.Node):
 
 @register.tag
 def extend(parser, token):
+    '''
+    Extends another template, passing additional variables to the parent.
+    '''
     bits = token.split_contents()
     kwargs = token_kwargs(bits[2:], parser)
     token.contents = " ".join(bits[:2])
-
-    # let the orginal do_extends parse the tag, and wrap the ExtendsNode
     return ExtendNode(do_extends(parser, token), kwargs)
 
 
 class MenuSectionNode(template.Node):
+    '''
+    Template node that renders a menu section.
+    '''
 
     def __init__(self, nodelist, title=None, is_list=None, link_name=None, id=None):
         self.nodelist = nodelist
@@ -105,9 +113,14 @@ def menu_section(parser, token):
 
 
 class AppMenuNode(template.Node):
+    '''
+    Template node that renders the main application menu for Nuit.
+    '''
+
     def __init__(self, nodelist, title=None):
         self.nodelist = nodelist
         self.title = title
+
     def render(self, context):
         content = self.nodelist.render(context)
         title = '<h5>%s</h5>' % self.title.resolve(context) if self.title else ''
@@ -115,6 +128,9 @@ class AppMenuNode(template.Node):
 
 @register.tag
 def app_menu(parser, token):
+    '''
+    Renders the main application menu for Nuit.
+    '''
     bits = token.split_contents()
     if len(bits) > 2:
         raise template.TemplateSyntaxError('Wrong number of arguments for app_menu - expected 2 maximum')
@@ -127,6 +143,9 @@ def app_menu(parser, token):
 
 @register.simple_tag
 def menu_item(link, name, id=None, current=False, unavailable=False):
+    '''
+    Renders an HTML anchor element in a list element.
+    '''
     if not id:
         id = slugify(name)
     classes = []
@@ -139,6 +158,9 @@ def menu_item(link, name, id=None, current=False, unavailable=False):
 
 @register.inclusion_tag('nuit/includes/_pagination_menu.html', takes_context=True)
 def pagination_menu(context, page_obj, show_totals=True):
+    '''
+    Renders the HTML required for the pagination options.
+    '''
     total_pages = page_obj.paginator.page_range
     actual_numbers = [page for page in total_pages if page >= total_pages[-1] - 1 or page <= 2 or (page >= page_obj.number - 2 and page <= page_obj.number + 2)]
     page_list = []
@@ -156,28 +178,56 @@ def pagination_menu(context, page_obj, show_totals=True):
 
 
 def calculate_widths(parts, num=12):
-       first = num // parts
-       last  = num - (parts - 1) * first
-       return (parts - 1) * [first] + [last]
+    '''
+    Generates a list of length parts containing integers, the sum of which is num.
+    '''
+    first = num // parts
+    last  = num - (parts - 1) * first
+    return (parts - 1) * [first] + [last]
 
 def split_strip(iter, delimiter=','):
+    '''
+    Splits an interator and returns each element stripped.
+    '''
     return [x.strip() for x in iter.split(delimiter) if x.strip()]
 
+
 class FoundationFormField(object):
-    def __init__(self, field, small=12, medium=None, large=None, prefix=None, postfix=None):
+    '''
+    A form field, containing Foundation-markup specific parameters.
+    '''
+
+    def __init__(self, field, small=12, medium=None, large=None, prefix=None, prefix_small=3, prefix_medium=None, prefix_large=None, postfix=None, postfix_small=3, postfix_medium=None, postfix_large=None):
         self.field = field
         self.small_width = small
         self.medium_width = medium or self.small_width
         self.large_width = large or self.medium_width
         self.prefix = prefix
+        self.prefix_small_width = prefix_small
+        self.prefix_medium_width = prefix_medium or self.prefix_small_width
+        self.prefix_large_width = prefix_large or self.prefix_medium_width
         self.postfix = postfix
+        self.postfix_small_width = postfix_small
+        self.postfix_medium_width = postfix_medium or self.postfix_small_width
+        self.postfix_large_width = postfix_large or self.postfix_medium_width
+
+        if self.prefix or self.postfix:
+            self.fix_field_small_width = 12 - (self.prefix_small_width if self.prefix else 0) - (self.postfix_small_width if self.postfix else 0)
+            self.fix_field_medium_width = 12 - (self.prefix_medium_width if self.prefix else 0) - (self.postfix_medium_width if self.postfix else 0)
+            self.fix_field_large_width = 12 - (self.prefix_large_width if self.prefix else 0) - (self.postfix_large_width if self.postfix else 0)
+
     def __repr__(self):
         return '<Field: %s (%d, %d, %d)>' % (self.field, self.small_width, self.medium_width, self.large_width)
 
 class FoundationFormNode(template.Node):
+    '''
+    A template node for a form field capabale of rendering Foundation-specific markup.
+    '''
+    
     def __init__(self, form, nodelist):
         self.form = form
         self.nodelist = nodelist
+
     def render(self, context):
         form = self.form.resolve(context)
         form_template = get_template('nuit/includes/_form.html')
@@ -201,17 +251,25 @@ class FoundationFormNode(template.Node):
                     except SyntaxError:
                         raise template.TemplateSyntaxError('Invalid parameters for field %s' % field_name)
 
-            sizes = ('small', 'medium', 'large',)
+            sizes = {
+                'field': ('small', 'medium', 'large',)
+            }
+            sizes['prefixes'] = tuple('prefix_%s' % s for s in sizes['field'])
+            sizes['postfixes'] = tuple('postfix_%s' % s for s in sizes['field'])
 
+            # For each field in the row, for each type of size, propagate any given sizes upwards.
             for x, y in row_data:
-                current_size = None
-                for size in sizes:
-                    if size in y:
-                        current_size = y[size]
-                    elif current_size:
-                        y[size] = current_size
+                for size_type in sizes.keys():
+                    current_size = None
+                    for size in sizes[size_type]:
+                        if size in y:
+                            current_size = y[size]
+                        elif current_size:
+                            y[size] = current_size
 
-            for size in sizes:
+            # For each size option, set the width properties of each field as defined.
+            # Default to all 12 for small sizes, and calculate any unspecified widths.
+            for size in sizes['field']:
                 total = sum(y[size] for x, y in row_data if size in y)
                 unspecified = len([x for x, y in row_data if size not in y or not y[size]])
                 if unspecified == len(row_data) and size == 'small':
@@ -223,9 +281,15 @@ class FoundationFormNode(template.Node):
                         continue
                     data[size] = unspecified_widths.pop(0)
 
-            layout_instructions.append(row_data)
+            # For each field, set the prefix/postfix widths if not defined.
+            for x, y in row_data:
+                for size_type in sizes.keys():
+                    if size_type == 'field': continue
+                    for size in sizes[size_type]:
+                        if size not in y or not y[size]:
+                            y[size] = 3
 
-        #layout_instructions = [split_strip(x, ',') for x in self.nodelist.render(context).splitlines() if x.strip()]
+            layout_instructions.append(row_data)
 
         all_fields = [field.name for field in form.visible_fields()]
 
@@ -246,11 +310,17 @@ class FoundationFormNode(template.Node):
                 remaining_fields.append(FoundationFormField(form[field]))
             field_layout.append(remaining_fields)
 
+        # Render the form. Template expects the form object, and a fields object
+        # which is a list of list, each list representing a row of the form.
+        # Each element in a row list is a FoundationFormField object.
         content = form_template.render(Context({'form': form, 'fields': field_layout}))
         return content
 
 @register.tag
 def foundation_form(parser, token):
+    '''
+    Renders a form with Foundation markup.
+    '''
     bits = token.split_contents()
     if len(bits) < 2:
         raise template.TemplateSyntaxError('Incorrect number of arguements for %s, Form object required' % bits[0])
