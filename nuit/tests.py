8a70c73a3bbea7f489f3133e6e43ed163dba4c94
Ben Cardy <ben.cardy@ocado.com>
2014-03-03 09:50:00 +0000

    A test!

--- nuit/tests.py
+++ nuit/tests.py
@@ -1,17 +1,19 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+'''Tests for nuit'''
 # pylint: disable=R0904
 from django.test import TestCase
+from .context_processors import nuit as nuit_context_processor
 
+class NuitContextProcessors(TestCase):
+    '''Tests Nuit's context processors'''
 
-class SimpleTest(TestCase):
-    '''Replace this class'''
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    sample_settings = {
+        'NUIT_APPLICATIONS': [{'name': 'Link', 'link': 'http://www.google.com'}],
+        'NUIT_GLOBAL_TITLE': 'Title',
+        'NUIT_LARGE_LOGO': 'logo.png',
+        'NUIT_SMALL_LOGO': 'small_logo.png',
+    }
+
+    def test_nuit_context_processor(self):
+        with self.settings(**self.sample_settings):
+            resulting_dict = nuit_context_processor(None)
+        self.assertEqual(resulting_dict, self.sample_settings)
