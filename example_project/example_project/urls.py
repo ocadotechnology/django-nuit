44f2a4242b6247ea487c4ee26a0291751ae81a09
Ben Cardy <ben.cardy@ocado.com>
2014-03-04 12:26:36 +0000

    Login view and docs

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^', include('demo.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'nuit/generic/login.html'}),
 )
