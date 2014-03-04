14765f7b05cd3259e22dae9f3631895c18cb15c4
Ben Cardy <ben.cardy@ocado.com>
2014-03-04 12:08:07 +0000

    Added a ROOTURL_CONF to test settings

--- test_settings.py
+++ test_settings.py
@@ -3,7 +3,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
     },
 }
-INSTALLED_APPS = ['nuit']
+ROOT_URLCONF = 'django_autoconfig.autourlconf'
+INSTALLED_APPS = ['nuit',]
 STATIC_URL = '/static/'
 from django_autoconfig.autoconfig import configure_settings
 configure_settings(globals())
