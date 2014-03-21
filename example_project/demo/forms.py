73ea4a928926a09087387509fa5f3819617e97d2
Ben Cardy <ben.cardy@ocado.com>
2014-03-21 12:17:57 +0000

    Added help_text to forms

--- example_project/demo/forms.py
+++ example_project/demo/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, ValidationError, CharField
 from .models import Publisher
 
 class PublisherForm(ModelForm):
+    name = CharField(help_text='The name of the publisher')
     class Meta:
         model = Publisher
         fields = ('name', 'address', 'city', 'state_province', 'country', 'website',)
