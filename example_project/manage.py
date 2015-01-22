2d24249e7832f68d4c0f897aa995ec9314ffefc2
Ben Cardy <ben.cardy@ocado.com>
2015-01-22 15:24:07 +0000

    Added app_logo and force escaped app_title. Fixes #4 and #5

--- example_project/manage.py
+++ example_project/manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
 
